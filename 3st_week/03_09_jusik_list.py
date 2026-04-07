30ebf51365e82b0dc73d70f3bcdb5b4d0d24dee3
seunggi-k <fdvc10@gmail.com>
2026-04-07 11:58:02 +0900

    3주차 완료

---
+++ 3st_week/03_09_jusik_list.py
@@ -0,0 +1,13 @@
+prices = [1, 2, 3, 2, 3]
+
+
+def get_price_not_fall_periods(prices):
+    # 이 부분을 채워주세요!
+    return
+
+
+print(get_price_not_fall_periods(prices))
+
+print("정답 = [4, 3, 1, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods(prices))
+print("정답 = [6, 2, 1, 3, 2, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods([3, 9, 9, 3, 5, 7, 2]))
+print("정답 = [6, 1, 4, 3, 1, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods([1, 5, 3, 6, 7, 6, 5]))
